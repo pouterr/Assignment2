907ec921fb5b7875bd53590256800e7c4a0ee1b6
pouterr <poutergutorov@gmail.com>
2023-04-18 23:33:35 +0600

    creating MyArrayList with implementing MyList interface

---
+++ src/MyArrayList.java
@@ -0,0 +1,57 @@
+public class MyArrayList<T> implements MyList<T>{
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
